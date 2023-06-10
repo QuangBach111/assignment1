91c3c3f0580dfadb07ae861b223e9deb21318387
Dung Bui <buiquangdung7497@gmail.com>
2023-06-10 19:59:08 +0700

    Revert "Revert "Merge pull request #5 from QuangBach111/developing""
    This reverts commit 69539e99f47a465b5092886ecc16db51580630dd.

---
+++ src/main/java/com/example/assignment1/controller/content/EditContentServlet.java
@@ -0,0 +1,32 @@
+package com.example.assignment1.controller.content;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+public class EditContentServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        // get id from req
+        int id = Integer.parseInt(req.getParameter("id"));
+
+        //get content from database by id
+
+        //set content to req
+
+        // forward to content-edit.jsp
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        // get param from req
+
+        // create content object
+
+        //stored to database
+
+    }
+}
